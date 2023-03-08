a8aee7b812d2e11a1e45963fdc9303708f01db11
Nikodem2121 <169744@student.uwm.edu.pl>
2023-03-08 17:55:30 +0100

    Zmiany w pliku

--- Zmienna.py
+++ Zmienna.py
@@ -7,3 +7,20 @@ d=3+4j
 print(d)
 e=b+c
 print(e)
+lista = ['a0',2,3,5,[7,6,5],5.5]
+print(lista[4])
+lista.append(6.5)
+print(lista)
+#dodanie elementu na pozycje
+lista_a=[3,6,8,3,2]
+lista_a.insert(3, 10)
+print(lista_a)
+#dodanie kilu elementów na koniec listy
+lista_b=[5,4,3,2,6]
+lista_b.extend([10, 12, 13, 14])
+print(lista_b)
+#usuwanie elementu po indeksie
+#usuwanie elementu po wartości elementu
+#odwrócenie listy
+#sortowanie
+

66d3e49ebf150b2163343eb07146944e291b2c74
Nikodem2121 <169744@student.uwm.edu.pl>
2023-03-08 18:21:43 +0100

    Dodanie słownika

--- Zmienna.py
+++ Zmienna.py
@@ -11,16 +11,41 @@ lista = ['a0',2,3,5,[7,6,5],5.5]
 print(lista[4])
 lista.append(6.5)
 print(lista)
+
 #dodanie elementu na pozycje
 lista_a=[3,6,8,3,2]
 lista_a.insert(3, 10)
 print(lista_a)
+
 #dodanie kilu elementów na koniec listy
-lista_b=[5,4,3,2,6]
+lista_b=[5,4,3,2,6,4,0,4]
 lista_b.extend([10, 12, 13, 14])
 print(lista_b)
-#usuwanie elementu po indeksie
-#usuwanie elementu po wartości elementu
-#odwrócenie listy
-#sortowanie
 
+#usuwanie elementu po indeksie
+del lista_b[2]
+print(lista_b)
+
+#usuwanie elementu po wartości elementu
+lista_b.remove(4)
+print(lista_b)
+
+#odwrócenie listy
+lista_a.reverse()
+print(lista_a)
+
+#sortowanie
+lista_a.sort()
+print(lista_a)
+
+slownik={'a':1,3:1,5:'b','a':5}
+print(slownik)
+print(slownik['a'])
+slownik['klucz']='wartość'
+print(slownik)
+
+slownik.pop('a')
+print(slownik)
+
+print(slownik.keys())
+print(slownik.values)
